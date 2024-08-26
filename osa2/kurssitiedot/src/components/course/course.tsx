2077423219f92c1ec7aa47240c25d376a50c2f3a
Roope Lehtinen <Roope98.RL@gmail.com>
2024-08-26 20:41:27 +0300

    feat: 2-5

--- osa2/kurssitiedot/src/components/course/course.tsx
+++ osa2/kurssitiedot/src/components/course/course.tsx
@@ -47,7 +47,7 @@ const Total = (props: { course: Course }) => {
   )
 }
 
-const Course = (props: CourseProps) => {
+export const Course = (props: CourseProps) => {
   return (
     <div>
       {props.courses.map((course) => (
@@ -60,58 +60,3 @@ const Course = (props: CourseProps) => {
     </div>
   )
 }
-
-const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ]
-
-  return (
-    <div>
-      <Course courses={courses} />
-    </div>
-  )
-}
-
-export default App
