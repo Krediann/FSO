f8e81dbeb17586d4ee42217d382115fd0902933b
Roope Lehtinen <Roope98.RL@gmail.com>
2024-08-26 21:56:04 +0300

    feat: 2-8 puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.tsx
+++ osa2/puhelinluettelo/src/App.tsx
@@ -3,34 +3,60 @@ import { useState } from "react"
 type ButtonEvent = React.FormEvent<HTMLButtonElement>
 type InputChangeEvent = React.ChangeEvent<HTMLInputElement>
 
+interface Person {
+  name: string
+  phoneNumber: string
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+  const [persons, setPersons] = useState<Person[]>([])
   const [newName, setNewName] = useState("")
+  const [newPhonenumber, setNewPhonenumber] = useState("")
 
   const handleSubmit = (event: ButtonEvent) => {
     event.preventDefault()
-    const existingPerson = persons.filter((person) => person.name === newName)
+    const existingPerson = persons.filter(
+      (person) =>
+        person.name === newName || person.phoneNumber === newPhonenumber
+    )
     if (existingPerson.length > 0) {
-      alert(`${newName} is already on the phonebook!`)
+      alert(
+        `Person: ${newName} or Phonenumber: ${newPhonenumber} is already on the phonebook!`
+      )
       setNewName("")
+      setNewPhonenumber("")
       return
     }
     const copy = [...persons]
-    copy.push({ name: newName })
+    copy.push({ name: newName, phoneNumber: newPhonenumber })
     setPersons(copy)
     setNewName("")
+    setNewPhonenumber("")
   }
 
-  const handleInputChange = (event: InputChangeEvent) => {
+  const handleNameInputChange = (event: InputChangeEvent) => {
     setNewName(event.target.value)
   }
+
+  const handlePhoneNumberInputChange = (event: InputChangeEvent) => {
+    setNewPhonenumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name:{" "}
-          <input type="text" value={newName} onChange={handleInputChange} />
+          Name:{" "}
+          <input type="text" value={newName} onChange={handleNameInputChange} />
+        </div>
+        <div>
+          Phonenumber:{" "}
+          <input
+            type="tel"
+            value={newPhonenumber}
+            onChange={handlePhoneNumberInputChange}
+          />
         </div>
         <div>
           <button onClick={handleSubmit} type="submit">
@@ -41,7 +67,9 @@ const App = () => {
       <h2>Numbers</h2>
       {persons.map((person) => (
         <div key={person.name}>
-          <p>{person.name}</p>
+          <p>
+            {person.name} {person.phoneNumber}
+          </p>
         </div>
       ))}
     </div>
