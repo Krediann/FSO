077824f31006171c2b10feafb0338014ce8e5a46
Roope Lehtinen <Roope98.RL@gmail.com>
2024-08-26 21:30:05 +0300

    feat: 2-6 puhelinluettelo step1

---
+++ osa2/puhelinluettelo/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+type ButtonEvent = React.FormEvent<HTMLButtonElement>
+type InputChangeEvent = React.ChangeEvent<HTMLInputElement>
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+  const [newName, setNewName] = useState("")
+
+  const handleSubmit = (event: ButtonEvent) => {
+    event.preventDefault()
+    const copy = [...persons]
+    copy.push({ name: newName })
+    setPersons(copy)
+    setNewName("")
+  }
+
+  const handleInputChange = (event: InputChangeEvent) => {
+    setNewName(event.target.value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name:{" "}
+          <input type="text" value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button onClick={handleSubmit} type="submit">
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>
+          <p>{person.name}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
