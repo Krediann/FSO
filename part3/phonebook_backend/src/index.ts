9f0a7c18592bb3a5f94430c63dd9ba800cedc7a4
Roope Lehtinen <Roope98.RL@gmail.com>
2024-09-09 22:36:24 +0300

    feat: 3-7 Phonebook backend step 7

--- part3/phonebook_backend/src/index.ts
+++ part3/phonebook_backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express"
+import morgan from "morgan"
 import { infoRouter } from "./modules/info/info.router"
 import { personsRouter } from "./modules/persons/persons.routes"
 
+const logger = morgan("tiny")
 const app = express()
 
 app.use(express.json())
+app.use(logger)
 
 app.use("/info", infoRouter)
 
